4e94ac83b295ea00b4535a755b804c9cf06b8a10
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-07-31 20:33:36 +0300

    mods

--- j_m/j_m.py
+++ j_m/j_m.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
 import sys
+import pathlib
+import os
 
-fname = sys.argv[1]
+DELIM = ","
 
-output = ""
 
-with open(fname, "r") as file:
-    for line in file.readlines():
-        spl = line.split(" ")
-        spl = filter(lambda v: v != "", spl)
-        output += ";".join(spl)
+def process_file(fname):
+    npath = fname.with_suffix(fname.suffix + ".csv")
+    data = ""
 
-print(output)
+    with open(fname, "r") as file:
+        for line in file.readlines():
+            spl = line.split(" ")
+            spl = filter(lambda v: v != "", spl)
+            data += DELIM.join(spl)
+
+    with open(npath, "w") as file:
+        file.write(data)
+
+
+def process_folder(fname):
+    for file in os.listdir(fname):
+        fpath = pathlib.Path(fname) / file
+        if fpath.suffix == ".j_m":
+            process_file(fpath)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("usage: python j_m.py <FILE>")
+
+    inpath = pathlib.Path(sys.argv[1])
+
+    if inpath.is_file():
+        process_file(inpath)
+
+    elif inpath.is_dir():
+        process_folder(inpath)
